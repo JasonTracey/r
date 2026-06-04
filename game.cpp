d98d547e2bf1e120694d74158298994fb1c83872
JasonTracey <53939731+JasonTracey@users.noreply.github.com>
2026-06-04 18:43:33 +0100

    Demo for suse

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 
 #include <unordered_set>
 #include <string>
+#include <iostream>
 #include "Location.h"
 #include "Unit.h"
 
@@ -18,5 +19,11 @@ int main () {
     Location forest("Forest");
     Species human("Human");
     Unit suse("Susannah", &human, &home);
+    
+    std::cout << suse.name() << " is at " << suse.location()->name() << '\n';
+    
     suse.go_to(&forest);
+    std::cout << suse.name() << " goes to " << forest.name() << '\n';
+    std::cout << suse.name() << " is at " << suse.location()->name() << '\n';
+    
 }
